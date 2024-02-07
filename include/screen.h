3f77e5be22dcf780c4b222c29d24a85cddfc6a46
Anatolii Charikov <pepsh6881@gmail.com>
2024-02-07 04:26:47 +0200

    the screen struct was added
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

--- include/screen.h
+++ include/screen.h
@@ -1,7 +1,14 @@
 #ifndef SCREEN_H_SENTRY
 #define SCREEN_H_SENTRY
 
-void
-clear_screen();
+typedef struct {
+	int begin_x;
+	int begin_y;
+	int end_x;
+	int end_y;
+} screen;
+
+void set_screen_parametres(void);
+void clear_screen(void);
 
 #endif
