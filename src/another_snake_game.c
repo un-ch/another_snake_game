4931de9e8643a41e848ddb4d55c3195d5e4e8f1e
Anatolii Charikov <pepsh6881@gmail.com>
2024-02-08 03:30:29 +0200

    fog_of_war object displaying was remove with regular barrier and target displaying
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

--- src/another_snake_game.c
+++ src/another_snake_game.c
@@ -48,8 +48,8 @@ int main()
 
 	while((direction_signal = getch()) != key_escape) {
 		handle_direction_signal(direction_signal, &coordinate, &snake);
-		display_target_in_fog_of_war(snake.first->coord, target);
-		display_barrier_in_fog_of_war(snake.first->coord, barrier);
+		display_target(target);
+		display_barrier(barrier);
 
 		if(is_contact_with_borders(snake.first->coord)
 		|| is_contact_with_barrier(snake.first->coord, barrier)) {
