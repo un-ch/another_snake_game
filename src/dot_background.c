5248143abbba3a27c06ff0c554e40db4093fa8d3
Anatolii Charikov <pepsh6881@gmail.com>
2024-02-08 02:43:29 +0200

    the code was rewritten with the global variable
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

--- src/dot_background.c
+++ src/dot_background.c
@@ -6,44 +6,39 @@
 #include "objects.h"
 #include "coordinates.h"
 
-void
-display_dot_background()
+extern screen g_screen;
+
+void display_dot_background()
 {
-	int max_screen_height, max_screen_width;
-	/* int min_screen_height, min_screen_width; */
-	int screen_height = 1;
-	int screen_width = 1;
+	int screen_pixel_x;
+	int screen_pixel_y = g_screen.begin_y + 1;
 	struct coordinates crd;
 
-	getmaxyx(stdscr, max_screen_height, max_screen_width);
-	set_color(black_on_white);
-
-	for(; screen_height < (max_screen_height - 1); ++screen_height) {
-		for(screen_width = 1; screen_width < (max_screen_width - 1); ++screen_width) {
-			crd.y = screen_height;
-			crd.x = screen_width;
+	for(; screen_pixel_y < (g_screen.end_y - 1); screen_pixel_y++) {
+		for(screen_pixel_x = g_screen.begin_x + 1; screen_pixel_x < (g_screen.end_x - 1); screen_pixel_x++) {
+			crd.y = screen_pixel_y;
+			crd.x = screen_pixel_x;
 			display_object(dot_obj, crd);
 		}
 	}
 }
 
-void
-display_dot_background_in_fog_of_war(const struct coordinates snake)
+void display_dot_background_in_fog_of_war(const struct coordinates snake)
 {
 	int x_max, x_min, y_max, y_min;
-	int screen_height, screen_width;
+	int screen_pixel_y, screen_pixel_x;
 
 	x_max = snake.x + 5;
 	x_min = snake.x - 5;
 	y_max = snake.y + 5;
 	y_min = snake.y - 5;
 
-	screen_height = y_min;
-	screen_width = x_min;
+	screen_pixel_y = y_min;
+	screen_pixel_x = x_min;
 
-	for(; screen_height < (y_max - 1); ++screen_height) {
-		for(screen_width = x_min; screen_width < (x_max - 1); ++screen_width) {
-			move(screen_height, screen_width);
+	for(; screen_pixel_y < (y_max - 1); ++screen_pixel_y) {
+		for(screen_pixel_x = x_min; screen_pixel_x < (x_max - 1); ++screen_pixel_x) {
+			move(screen_pixel_y, screen_pixel_x);
 			addch(symbol_dot);
 		}
 	}
