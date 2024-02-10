d5f80317be08d9c3c1406560f501b239f906c099
Anatolii Charikov <pepsh6881@gmail.com>
2024-02-10 15:17:30 +0200

    a few enus were changed
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

--- inc/objects.h
+++ inc/objects.h
@@ -1,35 +1,33 @@
-#ifndef OBJECTS_H_SENTRY
-#define OBJECTS_H_SENTRY
+#ifndef OBJECTS_H
+#define OBJECTS_H
 
 #include "coordinates.h"
 
-enum { symbol_target = '+' };
-enum { symbol_snake = '+' };
-enum { symbol_barrier = '#' };
-enum { symbol_blank = ' ' };
-enum { symbol_dot = '.' };
+enum {
+	symbol_target = '+',
+	symbol_snake = '+',
+	symbol_barrier = '#',
+	symbol_blank = ' ',
+	symbol_dot = '.'
+};
 
 enum object {
 	target_obj,
 	snake_obj,
 	barrier_obj,
 	blank_obj,
-	dot_obj
+	dot_obj,
+	game_title_obj
 };
 
-void 
-show_object_target(const struct coordinates crd);
+void show_object_target(const struct coordinates crd);
 
-void 
-show_object_snake(const struct coordinates crd);
+void show_object_snake(const struct coordinates crd);
 
-void 
-show_object_barrier(const struct coordinates crd);
+void show_object_barrier(const struct coordinates crd);
 
-void
-hide_object(const struct coordinates crd);
+void hide_object(const struct coordinates crd);
 
-void
-display_object(enum object obj, const struct coordinates crd);
+void display_object(enum object obj, const struct coordinates crd);
 
 #endif
