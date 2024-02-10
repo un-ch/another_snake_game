df50c43b8b0aef3bdc2ca25b1cdaa5da4116d682
Anatolii Charikov <pepsh6881@gmail.com>
2024-02-10 19:12:36 +0200

    enum was added
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

--- inc/snake_object.h
+++ inc/snake_object.h
@@ -3,6 +3,8 @@
 
 #include "user_types.h"
 
+enum { max_snake_length = 5 };
+
 void move_snake_object(coordinates_deque *snake, coordinates direction);
 void add_new_snake_element(coordinates_deque *snake, coordinates crd);
 
