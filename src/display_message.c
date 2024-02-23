2253c3ce05ef54cbbed4e8484c4126decaa64770
Anatolii Charikov <pepsh6881@gmail.com>
2024-02-23 19:26:55 +0200

    displaying the round message was updated
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

--- src/display_message.c
+++ src/display_message.c
@@ -1,10 +1,12 @@
 #include <ncurses.h>
 #include <unistd.h>
-
+#include "user_types.h"
 #include "display_message.h"
 #include "string.h"
 #include "game_field.h"
 #include "color.h"
+
+extern game_field g_game_field;
 
 void display_message_center(const char *string)
 {
@@ -23,15 +25,17 @@ void display_message_center(const char *string)
 
 void display_round_number(const int number)
 {
-	static const char *message = "Round";
-	int max_screen_value_y, max_screen_value_x;
-	int string_len;
+	const char *round_message = "Round";
+	const int string_len = string_length(round_message);
+	coordinates round_message_position;
 
-	getmaxyx(stdscr, max_screen_value_y, max_screen_value_x);
-	string_len = string_length(message);
-	move(max_screen_value_y / 2, (max_screen_value_x - string_len - 1) / 2);
-	set_color(black_on_white);
-	printw("%s %d", message, number);
+	round_message_position.x =
+		(g_game_field.end_x - string_len - g_game_field.begin_x) / 2;
+	round_message_position.y = g_game_field.end_y / 2;
+
+	set_color(green_on_black);
+
+	mvwprintw(stdscr, round_message_position.y, round_message_position.x, "%s %d", round_message, number);
 	refresh();
 	sleep(2);
 }
