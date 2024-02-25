51ee8226865e4f6feee280c3e4abd660ed0834cd
Anatolii Charikov <pepsh6881@gmail.com>
2024-02-25 17:44:07 +0200

    WINDOW variable was removed.
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

--- src/borders.c
+++ src/borders.c
@@ -10,30 +10,20 @@ extern game_field g_game_field;
 void display_borders_with_game_title(void)
 {
 	const char *game_title = "Simple Snake Game";
-	int border_lines_num, border_column_num;
 	const int title_length = string_length(game_title);
 	coordinates game_title_position;
 
 	set_color(magenta_on_black);
 	set_screen_parametres();
 
-	border_column_num = g_game_field.end_x - g_game_field.begin_x;
-
-	border_lines_num = g_game_field.end_y - g_game_field.begin_y;
-	WINDOW *game_border = newwin(
-		border_lines_num,
-		border_column_num,
-		g_game_field.begin_y,
-		g_game_field.begin_x);
-
-	box(game_border, 0, 0);
+	box(stdscr, 0, 0);
 	game_title_position.x =
-		(g_game_field.end_x - title_length - g_game_field.begin_x) / 2;
+		(getmaxx(stdscr) - title_length) / 2;
 	game_title_position.y = g_game_field.begin_y;
 
 	/* print the game title in the middle of the top border: */
-	mvwprintw(game_border, game_title_position.y, game_title_position.x, " %s ", game_title);
-	wrefresh(game_border);
+	mvwprintw(stdscr, game_title_position.y, game_title_position.x, " %s ", game_title);
+	wrefresh(stdscr);
 }
 
 int is_contact_with_borders(const coordinates snake_head)
